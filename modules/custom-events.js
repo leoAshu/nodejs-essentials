2bb74b47299bb716061ad3c330da6ebd4025f860
leoAshu <ashutosh.ojha2009@gmail.com>
2023-03-01 14:41:15 -0800

    refactors ask.js script into modules

---
+++ modules/custom-events.js
@@ -0,0 +1,18 @@
+const events = require("events");
+
+let emitter = new events.EventEmitter();
+
+emitter.on("customEvent", (message, user) => {
+    console.log(`${user}: ${message}`);
+});
+
+emitter.emit("customEvent", "Hello World!", "Computer");
+
+process.stdin.on("data", (data) => {
+    const input = data.toString().trim();
+    if(input === "exit") { 
+        emitter.emit("customEvent", "Goodbye!", "Process");
+        process.exit()
+    }
+    emitter.emit("customEvent", input, "Terminal User");
+});

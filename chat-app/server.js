48efa9a5397847ee9052036e13c8433fcc7427ed
leoAshu <ashutosh.ojha2009@gmail.com>
2023-03-02 14:09:34 -0800

    installs mongoose package and connects to mongodb database

--- chat-app/server.js
+++ chat-app/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const parser = require('body-parser');
 const app = express();
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(parser.urlencoded({extended: true}))
+
+const dbURL = 'mongodb+srv://ashu_leo:aXYUDQsIbFakXUm8@demo.mq7oizd.mongodb.net/?retryWrites=true&w=majority';
 
 var messages = [
     // {name: 'Tim', message: 'Hi'},
@@ -27,6 +30,16 @@ io.on('connection', (socket) => {
     console.log('a user connected');
 });
 
+mongoose.connect(dbURL)
+.then(() => {
+    console.log('db connected');
+}).catch((err) => {
+    console.log(err);
+});
+
 const server = http.listen(3000, () => {
     console.log('server running on port', server.address().port);
 });
+
+
+// aXYUDQsIbFakXUm8

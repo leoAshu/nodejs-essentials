6e3930977facc90d29e9f748aaeb630efc56f2fe
leoAshu <ashutosh.ojha2009@gmail.com>
2023-03-02 20:21:32 -0800

    writes two test specs for get call handler for chat app

--- testing/spec/server.spec.js
+++ testing/spec/server.spec.js
@@ -1,5 +1,28 @@
+const axios = require('axios');
+
 describe('calc', () => {
     it('should multiply 2 and 2', () => {
         expect(2*2).toBe(4);
     });
 });
+
+describe('get messages', () => {
+    it('should return 200 Ok', (done) => {
+        axios.get('http://localhost:3000/messages').then((res) => {
+            // console.log(res.data);
+            expect(res.status).toEqual(200);
+            done();
+        });
+    });
+
+    it('should return a non-empty list', (done) => {
+        axios.get('http://localhost:3000/messages').then((res) => {
+            // console.log(res);
+            expect(res.data.length).toBeGreaterThan(0);
+            done();
+        });
+    });
+});
+
+
+
